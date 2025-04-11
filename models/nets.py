cf1a0a7de2cb88290461ed1574f1a9e4859e1262
datht4889 <hoangdat18112003@gmail.com>
2025-04-11 09:08:40 +0700

    Update LInEx class to use configurable stream file path and comment out hardcoded file access

--- models/nets.py
+++ models/nets.py
@@ -146,7 +146,8 @@ class LInEx(MetaModule):
         self.trained_replay = set()
         self.trained_generate = set()
         # self.alpha = torch.nn.Parameter(torch.normal(torch.zeros(input_dim), torch.ones(input_dim)*-1)).to(self.device)
-        with open("data/MAVEN/streams.json") as f:
+        # with open("data/MAVEN/streams.json") as f:
+        with open(opts.stream_file) as f:
             task2id = json.load(f)
             id2task = {}
             start = 1
