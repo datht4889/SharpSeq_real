e817e372b5d4aeca54c535837663a0d906a6a5ed
datht4889 <hoangdat18112003@gmail.com>
2025-04-11 14:30:26 +0700

    Update options.py

--- utils/options.py
+++ utils/options.py
@@ -88,7 +88,7 @@ class Config:
     input_dim: int = 2048
     hidden_dim: int = 512   
     max_slots: int = 169
-    perm_id: int = 1
+    perm_id: int = 2
     
     # GPU settings.
     no_gpu: bool = False
